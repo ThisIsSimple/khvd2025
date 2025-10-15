87619056f14bdf60b10bbb0dd8f985d75be0b6a3
전윤민 <cordelia273@naver.com>
2025-10-15 21:13:00 +0900

    WIP

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), enhancedImages()],
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter()
 	}
