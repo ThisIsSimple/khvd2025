c58b4c3c76e0fdf2fbf1b7ab4db8ea246cebfe63
전윤민 <cordelia273@naver.com>
2025-11-06 17:53:36 +0900

    feat: old browser support

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
 		allowedHosts: ['khvd.kr', '2025.khvd.kr', 'dfbc1efd509a.ngrok-free.app']
+	},
+	build: {
+		target: ['es2015', 'safari11']
 	}
 });
